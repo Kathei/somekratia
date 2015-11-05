fec403d8f39dcd656cf7013ae78faa3ff9c75239
Emmi Linkola <emmi.linkola@aalto.fi>
2015-11-05 15:46:11 +0200

    search to work on click

--- app/static/navigation.js
+++ app/static/navigation.js
@@ -1,27 +1,38 @@
 /**
  * Created by emmilinkola on 03/11/15.
  */
-$(document).ready(function(){
-    var searchIssues = new SearchIssues();
 
-    /*var app = angular.module('myApp', ['ngRoute']);
-    app.controller('myCtrl', function($scope) {
+var searchIssues = new SearchIssues();
 
-    });
+var app = angular.module('myApp', ['ngRoute']);
+app.controller('myCtrl', function($scope) {
 
-    app.controller('searchController', function($scope, $http){
+});
+
+app.controller('searchController', function($scope, $http){
+
+    $scope.search = function(text) {
+
         console.log("test");
-        $http.get("http://dev.hel.fi/openahjo/v1/issue/search/?text=%s&format=json%s")
+        var config = {
+            'params' : {
+                'search': text,
+                'format': 'json',
+            },
+        };
+        $http.get("/issues/text/", config)
             .success(function(searchResult) {
                 console.log(searchResult);
-                $scope.names = searchResult.objects;
+                //$scope.names = searchResult.objects;
             });
-    });*/
+    }
 
-    $(".search-button").on("click", function(){
-        var searchInput = $("#search").val();
-        console.log(searchInput);
-        console.log("Moi");
-        searchIssues.handleSearch(searchInput);
-    });
+
 });
+
+/* $(".search-button").on("click", function(){
+ var searchInput = $("#search").val();
+ console.log(searchInput);
+ console.log("Moi");
+ searchIssues.handleSearch(searchInput);
+ });*/
